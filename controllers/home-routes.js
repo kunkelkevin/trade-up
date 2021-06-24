54ce14c7d0d27a426474d2a135e28aa4c45c9655
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-06-24 13:17:15 -0500

    home-route edits

---
+++ controllers/home-routes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Offer } = require("../models");
+
+// get all posts for homepage
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "console_type", "pic_link", "title", "description", "quality", "createdAt"],
+    include: [
+      {
+        model: Offer,
+        attributes: ["id", "description", "createdAt"],
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+          {
+            model: Comment,
+            attributes: ["id", "comment_text"],
+            include: {
+                model: User,
+                attributes: ["username"]
+            }
+          }
+        ]
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      //   res.json(posts);
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
