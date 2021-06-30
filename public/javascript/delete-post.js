ee7c4862bbe140117ee9b2e50860c1ba74506f55
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-06-30 07:16:25 -0500

    added changes css

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -4,7 +4,7 @@ async function deleteFormHandler(event) {
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-    const response = await fetch(`/api/post/${id}`, {
+    const response = await fetch(`/post/${id}`, {
       method: 'DELETE'
     });
   
